aeeab2db0f002bf88c28aa4012e073762a2f4304
Eduard Malakhov <eduard@atweel.com>
2020-04-27 04:35:57 +0000

    Advance tests

---
+++ package/src/components/Layered.spec.tsx
@@ -0,0 +1,34 @@
+import 'jest';
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import { Layered } from './Layered';
+
+describe(`Layered`, function () {
+    let container: HTMLDivElement;
+
+    beforeEach(() => {
+        // setup a DOM element as a render target
+        container = document.createElement('div');
+        document.body.appendChild(container);
+    });
+
+    afterEach(() => {
+        // cleanup on exiting
+        unmountComponentAtNode(container);
+        container.remove();
+    });
+
+    it(`produces a proper div element`, function () {
+        act(() => {
+            render(<Layered />, container);
+        });
+
+        const node = container.lastChild;
+
+        expect(node).toBeDefined();
+        expect(node).not.toBeNull();
+        expect(node?.nodeType).toBe(Node.ELEMENT_NODE);
+        expect((node as HTMLElement).tagName.toLowerCase()).toEqual('div');
+    });
+});
