b579635b19851cbd795f7a1d3bd208b2b731089b
Eduard Malakhov <eduard@atweel.com>
2020-04-28 04:12:38 +0000

    Advance merge driver for version json files

--- merge.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# diff <(jq .version=$(jq .version $1) $3 | jq -S .) <(jq . -S $1) ||
-
-jq .version=$(jq .version $1) $3 > $3.patched
-
-git merge-file $1 $2 $3.patched
-
-rm $3.patched
