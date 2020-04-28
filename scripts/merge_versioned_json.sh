e5cea1da1a4692c5ca1d014c80175669dae5b97a
Eduard Malakhov <eduard@atweel.com>
2020-04-28 05:51:58 +0000

    Add commnts to scripts/merge_versioned_json.sh

--- scripts/merge_versioned_json.sh
+++ scripts/merge_versioned_json.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
-# The arguments are the paths pointing to:
-#   the version of the file in the current branch
-#   the concestor of the current and the incoming branch
-#   the version of the file in the incoming branch
+#   This file attempts to implement a merge strategy for "versioned" JSON files like package.json.
+#   To use it as a git merge driver in a repository or globally, first set up the driver using
+#
+#       git config merge.versioned-json.driver $(realpath merge_versioned_json.sh)
+#
+#   or
+#
+#       git config --global merge.versioned-json.driver $(realpath merge_versioned_json.sh)
+#
+#   then associate the new driver with appropriate files using .gitattributes, e.g.
+#
+#       **/package.json merge=versioned-json
+#
+#   The script accepts three arguments:
+#       the file in the current branch,
+#       the nearest common ancestor of the files being merged, and
+#       the file in the incoming branch
+#
+#   Ths script relies on jq (https://stedolan.github.io/jq/) to manipulate JSON files.
 
 CURRENT_FILE_PATH=$1
 BASE_FILE_PATH=$2
@@ -11,9 +26,9 @@ INCOMING_FILE_PATH=$3
 
 echo -e "Trying to resolve merge conflicts in a versioned JSON file automatically..."
 
-ADJUSTED_INCOMING_FILE_PATH=$(mktemp /tmp/$INCOMING_FILE_PATH.XXXXXX)
+PRE_RESOLVED_INCOMING_FILE_PATH=$(mktemp /tmp/$INCOMING_FILE_PATH.XXXXXX)
 
-echo -e "Pre-resolved file will be written to $ADJUSTED_INCOMING_FILE_PATH"
+echo -e "Pre-resolved file will be written to $PRE_RESOLVED_INCOMING_FILE_PATH"
 
 CURRENT_VERSION=$(jq .version $CURRENT_FILE_PATH)
 
@@ -23,11 +38,11 @@ INCOMING_VERSION=$(jq .version $INCOMING_FILE_PATH)
 
 echo -e "Incoming version is $INCOMING_VERSION"
 
-jq .version=$(jq .version $CURRENT_FILE_PATH) $INCOMING_FILE_PATH > $ADJUSTED_INCOMING_FILE_PATH
+jq .version=$(jq .version $CURRENT_FILE_PATH) $INCOMING_FILE_PATH > $PRE_RESOLVED_INCOMING_FILE_PATH
 
-echo -e "After resolving version conflict, the diff is:\n$(diff $CURRENT_FILE_PATH $ADJUSTED_INCOMING_FILE_PATH)"
+echo -e "After resolving version conflict, the diff is:\n$(diff $CURRENT_FILE_PATH $PRE_RESOLVED_INCOMING_FILE_PATH)"
 
-git merge-file $CURRENT_FILE_PATH $BASE_FILE_PATH $ADJUSTED_INCOMING_FILE_PATH
+git merge-file $CURRENT_FILE_PATH $BASE_FILE_PATH $PRE_RESOLVED_INCOMING_FILE_PATH
 
 MERGE_RESULT=$?
 
@@ -38,6 +53,6 @@ else
     echo -e "Merge Failed"
 fi
 
-rm $ADJUSTED_INCOMING_FILE_PATH
+rm $PRE_RESOLVED_INCOMING_FILE_PATH
 
 exit $MERGE_RESULT
